e48d65b59ad9bacbd17f2064ac5bd0e50d4f4c61
joseluisvazzquez <jvazquez3@nigul.cide.es>
2024-03-21 19:15:19 +0100

    juego

--- t2/GUI_Battleship/Battleship.java
+++ t2/GUI_Battleship/Battleship.java
@@ -3,14 +3,11 @@ package GUI_Battleship;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
- 
 import javax.swing.SwingUtilities;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
  
 
--- t2/GUI_Battleship/BattleshipGame.java
+++ t2/GUI_Battleship/BattleshipGame.java
@@ -1,27 +1,53 @@
 package GUI_Battleship;
 
+import java.awt.Color;
 import java.awt.GridLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.ArrayList; 
+import java.util.List;
 import javax.swing.JButton; 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class BattleshipGame extends JFrame {
     private int table = 5;
-    private JButton buttons;
-
+    private int numBarcos = 5;
 
     public BattleshipGame() {
+        pack();
+        setVisible(true);
+        setLayout(new GridLayout(table,table));
+        List<JButton> buttonList = new ArrayList<JButton>();
+        Random r = new Random();
+        
         for(int i = 0; i < table*table; i++){
-            buttons = new JButton();
-            add(buttons);
-            buttons.setSize(30,30);
+            JButton button = new JButton(String.valueOf(i));
+            buttonList.add(button);
+            add(button);
+            
+            button.setSize(30,30);
+
+            button.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                        
+                        for(int j = 0; j<5; j++){
+                            int random1 = r.nextInt(table*table) -1;
+                            if(button.getText().equals(String.valueOf(random1))){
+                                button.setBackground(Color.GREEN);
+                            }else{
+                                button.setBackground(Color.BLUE);
+                            }
+                        }
+                    
+                }
+                
+            });
         }
         
-        setLayout(new GridLayout(table,table));
-        setVisible(true);
-        pack();
-
+        
     }
     
 }
